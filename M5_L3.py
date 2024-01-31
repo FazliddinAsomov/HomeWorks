1d175f75ef9bfd4cecefb33bec1ce8293f121b94
Fazliddin <fazliddin71539@gmail.com>
2024-02-01 01:20:15 +0500

    2.0 New file created

--- M5_L3.py
+++ M5_L3.py
@@ -12,3 +12,11 @@ elif o_rta_arifmetik < o_rta_geometrik:
     print("<")
 else:
     print('=')
+# Isfandiyor algebra darsida
+def calculate_f(n):
+    result = n**5 + 8*n**4 - 5*n**3 + 3*n**2 + n - 12
+    return result
+with open('input.txt', 'r') as input_file:
+    n = int(input_file.readline().strip())
+with open('output.txt', 'w') as output_file:
+    output_file.write(str(calculate_f(n)))
